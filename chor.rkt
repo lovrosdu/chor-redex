80719882e64a660e8942a360ef92f9ebea8e6f03
lovrosdu <lugovic@imada.sdu.dk>
2023-05-11 13:29:11 +0200

    Add recursive choreographies and well-formedness

--- chor.rkt
+++ chor.rkt
@@ -4,25 +4,35 @@
 (require racket/date)
 (require redex)
 
-;; * Terminology
+;; * Abbreviations
+;;
+;; - chor -- choreography
+;; - cstore -- choreographic store
+;; - pstore -- process store
+;; - dstore -- set of definitions (procedures)
+;; - conf -- configuration
 ;;
 ;; - sc -- Simple Choreographies
 ;; - st -- Stateful Choreographies
 ;; - cc -- Conditional Choreographies
 ;; - sl -- Selective Choreographies
+;; - rc -- Recursive Choreographies
+;;
+;; - wf -- well-formed
 ;; - pn -- process names
-;; - cstore -- choreographic store
-;; - pstore -- process store
+;; - p -- process
+;; - i -- instruction
+;; - c -- choreography
+;; - d -- definition
+;; - f -- configuration
 
 ;; * TODO
 ;;
 ;; - Proper tests. Small examples are in the comments below.
 ;;
-;; - RecursiveChor.
-;;
 ;; - Chapter 10.
 ;;
-;; - Basic typing.
+;; - Well-formedness for the runtime term.
 
 ;;; Util
 
@@ -47,6 +57,9 @@
 (define (applify proc)
   (lambda (args)
     (apply proc args)))
+
+(define (partial proc . fixed)
+  (lambda args (apply proc (append fixed args))))
 
 ;;; SimpleChor
 
@@ -124,13 +137,13 @@
 
 (define (assoc-store k store [default (void)])
   (match store
-    [`(,(or 'cstore 'pstore) . ,alist)
+    [`(,(or 'cstore 'pstore 'dstore) . ,alist)
      (let ([v (assoc k alist)])
        (if v (second v) default))]))
 
 (define (put-store k v store)
   (match store
-    [`(,(and store (or 'cstore 'pstore)) . ,alist)
+    [`(,(and store (or 'cstore 'pstore 'dstore)) . ,alist)
      `(,store ,@(aput k (list v) alist #:less? symbol<?))]))
 
 (define-metafunction StatefulChor
@@ -385,3 +398,226 @@
 
 ;; (traces SelectiveChor-> (term (conf C6-16 Σ6-16)))
 ;; (traces SelectiveChor-> (term (conf (chor (p → q [ok])) (cstore))))
+
+;;; RecursiveChor
+
+(define-extended-language RecursiveChor SelectiveChor
+  ;; Definitions
+  (X ::= id)
+  (D ::= (dstore (X ((p ...) C)) ...))
+  ;; Choreographies
+  (I ::=
+     ....
+     (X p ...)
+     (enter (p ...) (X p ...) C))
+  ;; Configurations
+  (Conf ::= (conf C Σ D)))
+
+(define-metafunction RecursiveChor
+  subst-p : p (p p) ... -> p
+  [(subst-p p (p_1 q_1) ... (p q) (p_2 q_2) ...) q]
+  [(subst-p p _ ...) p])
+
+(define-metafunction RecursiveChor
+  subst-i : I (p p) ... -> I
+  [(subst-i (p e → q x) (p_1 q_1) ...)
+   ((subst-p p (p_1 q_1) ...) e → (subst-p q (p_1 q_1) ...) x)]
+  [(subst-i (p x := e) (p_1 q_1) ...)
+   ((subst-p p (p_1 q_1) ...) x := e)]
+  [(subst-i (if (p e) (chor I_1 ...) (chor I_2 ...)) (p_1 q_1) ...)
+   (if ((subst-p p (p_1 q_1) ...) e)
+       (chor (subst-i I_1 (p_1 q_1) ...) ...)
+       (chor (subst-i I_2 (p_1 q_1) ...) ...))]
+  [(subst-i (p → q [l]) (p_1 q_1) ...)
+   ((subst-p p (p_1 q_1) ...) → (subst-p q (p_1 q_1) ...) [l])]
+  [(subst-i (X p ...) (p_1 q_1) ...)
+   (X (subst-p p (p_1 q_1) ...) ...)])
+
+(define-metafunction/extension sl-pn-i RecursiveChor
+  rc-pn-i : I -> (p ...)
+  [(rc-pn-i (X p ...)) (p ...)]
+  [(rc-pn-i (enter (q ...) _ _)) (q ...)])
+
+(define-metafunction RecursiveChor
+  get-def : D X -> ((p ...) C)
+  [(get-def D X) ,(apply assoc-store (term (X D)))])
+
+(define-metafunction RecursiveChor
+  put-def : D (X p ...) C -> D
+  [(put-def D (X p ...) C) ,(apply put-store (term (X ((p ...) C) D)))])
+
+(define-judgment-form RecursiveChor
+  #:mode (rc→ I O O)
+  #:contract (rc→ Conf μ Conf)
+  [(↓ (get-pstore Σ p) e v)
+   --------------------------------------------- local
+   (rc→ (conf (chor (p x := e) I ...) Σ D)
+        (τ @ p)
+        (conf (chor I ...) (put-var Σ p x v) D))]
+  [(↓ (get-pstore Σ p) e v)
+   --------------------------------------------- com
+   (rc→ (conf (chor (p e → q x) I ...) Σ D)
+        (p v → q)
+        (conf (chor I ...) (put-var Σ q x v) D))]
+  [---------------------------------------- sel
+   (rc→ (conf (chor (p → q [l]) I ...) Σ D)
+        (p → q [l])
+        (conf (chor I ...) Σ D))]
+  [(↓ (get-pstore Σ p) e #t)
+   ---------------------------------------------------------- cond-then
+   (rc→ (conf (chor (if (p e) (chor I_1 ...) C_2) I ...) Σ D)
+        (τ @ p)
+        (conf (chor I_1 ... I ...) Σ D))]
+  [(↓ (get-pstore Σ p) e #f)
+   ----------------------------------------------------------- cond-else
+   (rc→ (conf (chor (if (p e) C_1 (chor I_2 ...)) I ...) Σ D)
+        (τ @ p)
+        (conf (chor I_2 ... I ...) Σ D))]
+  [(where ((q ...) (chor I_1 ...)) (get-def D X))
+   (where (p_1 ... p_2 p_3 ...) (p ...))
+   ----------------------------------------------------------------- call-first
+   (rc→ (conf (chor (X p ...) I ...) Σ D)
+        (τ @ p_2)
+        (conf (chor (enter (p_1 ... p_3 ...) (X p ...) (chor I ...))
+                    (subst-i I_1 (q p) ...) ...
+                    I ...)
+              Σ D))]
+  [(where (q_1 ... q_2 q_3 ...) (q ...))
+   (side-condition ,(> (length (term (q ...))) 1))
+   ------------------------------------------------------------------- call-enter
+   (rc→ (conf (chor (enter (q ...) (X p ...) C) I ...) Σ D)
+        (τ @ q_2)
+        (conf (chor (enter (q_1 ... q_3 ...) (X p ...) C) I ...) Σ D))]
+  [---------------------------------------------------- call-last
+   (rc→ (conf (chor (enter (q) (X p ...) C) I ...) Σ D)
+        (τ @ q)
+        (conf (chor I ...) Σ D))]
+  [(rc→ (conf (chor I_1 ...) Σ_1 D) μ (conf (chor I_2 ...) Σ_2 D))
+   (side-condition ,(apply set-disjoint? (term ((rc-pn-i I) (sl-pn-μ μ)))))
+   ------------------------------------------------------------------------ delay
+   (rc→ (conf (chor I I_1 ...) Σ_1 D) μ (conf (chor I I_2 ...) Σ_2 D))]
+  [(rc→ (conf C_1 Σ_1 D) μ (conf C_2 Σ_2 D))
+   (rc→ (conf C_3 Σ_1 D) μ (conf C_4 Σ_2 D))
+   (side-condition ,(apply set-disjoint? (term ((p) (sl-pn-μ μ)))))
+   ---------------------------------------------------------------- delay-cond
+   (rc→ (conf (chor (if (p e) C_1 C_3) I ...) Σ_1 D)
+        μ
+        (conf (chor (if (p e) C_2 C_4) I ...) Σ_2 D))])
+
+(define RecursiveChor->
+  (reduction-relation
+   RecursiveChor
+   #:domain Conf
+   (--> Conf_1 Conf_2
+        (judgment-holds (rc→ Conf_1 μ Conf_2))
+        (computed-name (term (sl-format-μ μ))))))
+
+(define-term C-ping
+  (chor (Ping Alice Bob)))
+
+(define-term D-ping
+  (put-def (dstore) (Ping p q) (chor (p → q [sig]) (Ping p q))))
+
+;; (traces RecursiveChor-> (term (conf C-ping (cstore) D-ping)))
+
+(define-term C-ping-pong
+  (chor (Alice → Bob [sig]) (PP Alice Bob)))
+
+(define-term D-ping-pong
+  (put-def (dstore) (PP p q) (chor (p → q [sig]) (PP q p))))
+
+;; (traces RecursiveChor-> (term (conf C-ping-pong (cstore) D-ping-pong)))
+
+(define (make-packet file n)
+  (list (list-ref file (sub1 n))))
+
+(define (read-file filename)
+  (case filename
+    [("motd") (list "hello" "there" "world")]
+    [else (error 'read-file "no file named ~s" filename)]))
+
+(define empty-file list)
+(define packets length)
+(define crc identity)
+
+(define-term C7-2
+  (chor (downloader "motd" → storage filename)
+        (storage file := (read-file filename))
+        (downloader file := (empty-file))
+        (storage n := 1)
+        (S downloader storage)
+        (storage (crc file) → downloader crc-orig)
+        (downloader ok := (equal? (crc file) crc-orig))))
+
+(define-term D7-2
+  (put-def (dstore)
+           (S c s)
+           (chor (if (s (<= n (packets file)))
+                     (chor (s → c [next])
+                           (s (make-packet file n) → c packet)
+                           (c file := (append file packet))
+                           (s n := (+ n 1))
+                           (S c s))
+                     (chor (s → c [end]))))))
+
+;; (traces RecursiveChor-> (term (conf C7-2 (cstore) D7-2)))
+
+(define-judgment-form RecursiveChor
+  #:mode (rc-wf-c I O I)
+  #:contract (rc-wf-c D (p ...) C)
+  [------------------- wf-end
+   (rc-wf-c _ () (chor))]
+  [(rc-wf-c D (p_1 ...) (chor I ...))
+   (side-condition (not (apply equal? (term (p q)))))
+   (where (p_2 ...) ,(apply set-union (term ((p q) (p_1 ...)))))
+   ------------------------------------------------------------- wf-com
+   (rc-wf-c D (p_2 ...) (chor (p e → q x) I ...))]
+  [(rc-wf-c D (p_1 ...) (chor I ...))
+   (side-condition (not (apply equal? (term (p q)))))
+   (where (p_2 ...) ,(apply set-union (term ((p q) (p_1 ...)))))
+   ------------------------------------------------------------- wf-sel
+   (rc-wf-c D (p_2 ...) (chor (p → q [l]) I ...))]
+  [(rc-wf-c D (p_1 ...) (chor I ...))
+   (where (p_2 ...) ,(apply set-union (term ((p) (p_1 ...)))))
+   ----------------------------------------------------------- wf-local
+   (rc-wf-c D (p_2 ...) (chor (p x := e) I ...))]
+  [(rc-wf-c D (p_1 ...) C_1)
+   (rc-wf-c D (p_2 ...) C_2)
+   (rc-wf-c D (p_3 ...) (chor I ...))
+   (where (p_4 ...)
+          ,(apply set-union (term ((p) (p_1 ...) (p_2 ...) (p_3 ...)))))
+   --------------------------------------------------------------------- wf-cond
+   (rc-wf-c D (p_4 ...) (chor (if (p e) C_1 C_2) I ...))]
+  [(where ((q ...) _) (get-def D X))
+   (rc-wf-c D (p_1 ...) (chor I ...))
+   (side-condition ,(= (length (term (p ...))) (length (term (q ...)))))
+   (side-condition ,(not (check-duplicates (term (p ...)))))
+   (where (p_2 ...) ,(apply set-union (term ((p ...) (p_1 ...)))))
+   --------------------------------------------------------------------- wf-call
+   (rc-wf-c D (p_2 ...) (chor (X p ...) I ...))])
+
+(define-judgment-form RecursiveChor
+  #:mode (rc-wf-d I O)
+  #:contract (rc-wf-d D ((X (p ...)) ...))
+  [(rc-wf-c D (p ...) C)
+   ...
+   (side-condition ,(andmap (compose not check-duplicates)
+                            (term ((q ...) ...))))
+   (side-condition ,(andmap (applify subset?) (term (((p ...) (q ...)) ...))))
+   (side-condition ,(andmap (compose (partial < 1) length)
+                            (term ((q ...) ...))))
+   ---------------------------------------------------------------------------
+   (rc-wf-d (name D (dstore (X ((q ...) C)) ...)) ((X (p ...)) ...))])
+
+(define-judgment-form RecursiveChor
+  #:mode (rc-wf-f I O)
+  #:contract (rc-wf-f Conf (p ...))
+  [(rc-wf-c D (p ...) C)
+   (rc-wf-d D _)
+   ------------------------------
+   (rc-wf-f (conf C _ D) (p ...))])
+
+;; (judgment-holds (rc-wf-c D-ping any C-ping) any)
+;; (judgment-holds (rc-wf-d D-ping any) any)
+;; (judgment-holds (rc-wf-d (dstore (X ((p) (chor (p x := e))))) any) any)
+;; (judgment-holds (rc-wf-f (conf C-ping (cstore) D-ping) any) any)
